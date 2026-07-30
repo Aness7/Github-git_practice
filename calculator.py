147fd5699b02fb77237610dfcc9bd0e0e0dd18bf
Aness <147276169+Aness7@users.noreply.github.com>
2026-07-30 22:57:21 +0100

    operations integration

--- calculator.py
+++ calculator.py
@@ -9,3 +9,11 @@ print("The sum is: " + add)
 #Subtraction operation
 sub= a - b
 print("The difference is: " + sub)
+
+#Multiplication operation
+mul= a * b
+print("The product is: " + mul)
+
+#Division operation
+div= a / b
+print("The quotient is: " + div)
