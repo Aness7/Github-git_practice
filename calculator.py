1d0f9bfe8566e6f9b2c75817302590235778aa8c
Aness <147276169+Aness7@users.noreply.github.com>
2026-07-30 19:39:02 +0100

    w01: implement subtraction

---
+++ calculator.py
@@ -0,0 +1,11 @@
+print("Welcome to the calculator program")
+
+#Addition operation
+a= int(input("Enter first number: "))
+b= int(input("Enter second number: "))
+add= a + b    
+print("The sum is: " + add)
+
+#Subtraction operation
+sub= a - b
+print("The difference is: " + sub)
